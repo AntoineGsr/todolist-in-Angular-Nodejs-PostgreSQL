6b2a5432087fe2e56057a9f8e6adc12f7ebe2bff
Antoine Gasser <antoine.gasser@epitech.eu>
2023-04-04 11:53:58 +0200

    Angular project to transfer on both account

---
+++ back/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Configuration de la connexion à la base de données
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'angular-train',
+  password: 'antoineDragHerag1',
+  port: 5432,
+});
+
+//Vérification connexion à la DB
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Erreur lors de la connexion à la base de données', err);
+  } else {
+    console.log('Connecté à la base de données PostgreSQL');
+  }
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Route pour récupérer toutes les tâches
+app.get('/tasks', (req, res) => {
+  pool.query('SELECT * FROM public.tasks', (err, results) => {
+    if (err) throw err;
+    res.send(results.rows);
+  });
+});
+
+// Route pour ajouter une tâche
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  pool.query('INSERT INTO tasks (name) VALUES ($1)', [task.name], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// Route pour mettre à jour une tâche
+app.put('/tasks/:id', (req, res) => {
+  const task = req.body;
+  pool.query('UPDATE tasks SET name = $1, status = $2 WHERE id = $3', [task.name, task.status, task.id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// Route pour supprimer une tâche
+app.delete('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  pool.query('DELETE FROM tasks WHERE id = $1', [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// Récupération des tâches en fonction du statut
+app.get('/tasks/getTaskByStatus/:idStatus', (req, res) => {
+  const idStatus = req.params.idStatus;
+  const sql = 'SELECT * FROM tasks WHERE status = $1';
+  pool.query('SELECT * FROM tasks WHERE status = $1', [idStatus], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la récupération des tâches par statut');
+    } else {
+      res.status(200).json(results.rows);
+    }
+  });
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ front/src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+import { Task } from 'src/app/model/task';
+import { catchError, tap } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  taskObj: Task = new Task();
+  taskArr1: Task[] = [];
+  taskArr2: Task[] = [];
+  taskArr3: Task[] = [];
+  addTaskValue = '';
+  editTaskValue = '';
+
+  constructor(private crudService: CrudService) {}
+
+  ngOnInit(): void {
+    this.editTaskValue = '';
+    this.addTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr1 = [];
+    this.taskArr2 = [];
+    this.taskArr3= [];
+    this.getAllTask();
+  }
+
+  addTask() {
+    this.taskObj.name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).pipe(
+      tap(() => {
+        this.ngOnInit();
+        this.addTaskValue = '';
+      }),
+      catchError((err) => {
+        alert("Error: addTask");
+        console.log("Error: addTask");
+        return EMPTY;
+      })
+    ).subscribe();
+  }
+
+  getAllTask() {
+    this.getTaskByStatus(0, this.taskArr1);
+    this.getTaskByStatus(1, this.taskArr2);
+    this.getTaskByStatus(2, this.taskArr3);
+  }
+
+  editTask() {
+    this.taskObj.name = this.editTaskValue;
+    console.log(this.taskObj.id, this.taskObj.name);
+    this.crudService.editTask(this.taskObj).pipe(
+      tap(() => {
+        this.ngOnInit();
+      }),
+      catchError((err) => {
+        alert('Erreur: editTask');
+        console.log('Erreur: editTask');
+        return EMPTY;
+      })
+    ).subscribe();
+  }
+
+  deleteTask(etask: Task) {
+    this.crudService.deleteTask(etask).pipe(
+      tap(() => {
+        this.ngOnInit();
+      }),
+      catchError((err) => {
+        alert('Erreur: deleteTask');
+        console.log('Erreur: deleteTask');
+        return EMPTY;
+      })
+    ).subscribe();
+  }
+
+  call(etask: Task) {
+    this.taskObj = etask;
+    this.editTaskValue = etask.name;
+  }
+
+  getTaskByStatus(idStatus : number, taskArr : Task[]) {
+    this.crudService.getTaskByStatus(idStatus).pipe(
+      tap((res) => {
+        if (idStatus == 0)
+          this.taskArr1 = res;
+        if (idStatus == 1)
+          this.taskArr2 = res;
+        if (idStatus == 2)
+          this.taskArr3 = res;
+      }),
+      catchError((err) => {
+        alert('Erreur: getTaskByStatus');
+        console.log('Erreur: getTaskByStatus');
+        return EMPTY;
+      })
+    ).subscribe();
+  }
+}
